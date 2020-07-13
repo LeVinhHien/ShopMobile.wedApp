ff164b06670c00ec2e682303e49d50360b9b3dd5
Le Hien <clasofclan.98@outlook.com.vn>
2020-07-13 15:10:05 +0700

    Inital solution and repository

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopMobile.wedApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
